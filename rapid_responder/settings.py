cf6c473c3addc4f5798c9b371b78da604757aa48
ynjacobs <ytj64611@gamil.com>
2019-08-05 15:31:14 -0400

    init

--- rapid_responder/settings.py
+++ rapid_responder/settings.py
@@ -136,6 +136,9 @@ CORS_ORIGIN_WHITELIST = (
     'http://0.0.0.0:3000',
     'http://localhost:3000',
     'http://www.rapid-responder.com:3000',
+    'https://rapid-responder-api.herokuapp.com/',
+    'https://rapid-responder.netlify.com/',
+
 
 )
 
