aec03bf939d49ea9307c8407a4d56d9bfc31dcc7
ynjacobs <ytj64611@gamil.com>
2019-08-06 12:18:57 -0400

    static

--- rapid_responder/settings.py
+++ rapid_responder/settings.py
@@ -172,7 +172,7 @@ SIMPLE_JWT = {
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_URL = '/static/'    
-
+STATIC_URL = os.path.join(BASE_DIR, 'static')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
